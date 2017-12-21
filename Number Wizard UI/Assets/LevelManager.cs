648f2448dab1d0cc32340ae3f9b729c618f0aa8e
George Tourtsinakis <george@besmartbesimple.com>
2017-12-21 23:26:31 +0200

    Scenes created

---
+++ Number Wizard UI/Assets/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name)
+	{
+		//Write to console that the click works
+		Debug.Log ("Level load requested for:" + name);
+
+		//Load level according to parameter
+		Application.LoadLevel(name);
+	}
+
+	public void QuitRequest()
+	{
+		Debug.Log ("Quit requested ");
+		Application.Quit ();
+	}
+
+
+}
