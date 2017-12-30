89feb315f32542cfffcb4b84b227ef44ea234589
George Tourtsinakis <george@besmartbesimple.com>
2017-12-30 19:50:39 +0200

    Music added to Arkanoid Nostalgia and Fix for Number Wizard UI
    - Music added with loop and runs in every scene with GameObject.DontDestroyOnLoad() for Arkanoin Nostalgia
    - Number Wizard UI loads guess text after wizard's speech

---
+++ Arkanoid Nostalgia/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private static MusicPlayer instance = null;
+
+     void Awake()
+     {
+        
+        ////If any Music Player exists destroy it else continue in next scenes
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("Duplicate music player self-destructing");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+  
+
+    // Use this for initialization
+    void Start () {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    
+
+}
--- Number Wizard UI/Assets/NumberWizard.cs
+++ Number Wizard UI/Assets/NumberWizard.cs
@@ -10,12 +10,16 @@ public class NumberWizard : MonoBehaviour {
     int maxGuessesAllowed;
 
     //Controlling the text
-    public Text text;
+    public Text speechBubble;
+    public Text correct;
+    public Text guessHigher;
+    public Text guessLower;
 
 
-	// Use this for initialization
-	void Start () {
-		StartGame ();
+    // Use this for initialization
+    void Start () {
+        StartingText();
+        StartGame ();
 	}
 
 	public void StartGame()
@@ -23,20 +27,23 @@ public class NumberWizard : MonoBehaviour {
 		//We must have these values so we can start using where the number is between
 		max = 1000;
 		min = 1;
-        maxGuessesAllowed = 5;
+        maxGuessesAllowed = 10;
         //Gives a random number at start instead of a fixed
         guess = Random.Range (1, 1000);
-        StartCoroutine(WaitText());
         
+        StartCoroutine(WaitText());
+        StartCoroutine(ButtonsAfterSpeech());
 		//Fix if they choose 1000
 		max = max + 1;
-	}
+        
+
+    }
 
 
 	void NextGuess()
 	{
 		guess = (max + min)/2;
-        text.text = Guess();
+        speechBubble.text = Guess();
         maxGuessesAllowed = maxGuessesAllowed -  1;
         Debug.Log(maxGuessesAllowed);
         if (maxGuessesAllowed <= 0)
@@ -64,16 +71,35 @@ public class NumberWizard : MonoBehaviour {
     IEnumerator WaitText()
     {
             yield return new WaitForSeconds(3.0f);
-            text.fontSize = 28;
-            text.text = "Choose a number from 1 - 1000 \n and I'll find it";
+            speechBubble.fontSize = 28;
+            speechBubble.text = "Choose a number from 1 - 1000 \n and I'll find it";
             yield return new WaitForSeconds(5.0f);
-            text.fontSize = 40;
-            text.text = Guess();
+            speechBubble.fontSize = 40;
+            speechBubble.text = Guess();
     }
 
     public string Guess()
     {
         return "Is your number \n" + guess + "?";
+    }
+
+    IEnumerator ButtonsAfterSpeech()
+    {
+        
+        yield return new WaitForSeconds(10f);
+        correct.text = "Guessed Corect";
+        guessHigher.text = "Guess Higher";
+        guessLower.text = "Guess Lower";
+
+    }
+
+    public void StartingText()
+    {
+        speechBubble.text= "I think I can\n read minds";
+        correct.text = "";
+        guessHigher.text = "";
+        guessLower.text = "";
+
     }
 
 
