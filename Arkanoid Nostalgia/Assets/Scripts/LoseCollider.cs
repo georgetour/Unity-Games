98c8e89d1594707ab69f87ccbe2a32415eb0449b
George Tourtsinakis <george@besmartbesimple.com>
2017-12-31 16:46:47 +0200

    Arkanoid Nostalgia collisions
    - Added collision for ball that drops out of the screen

---
+++ Arkanoid Nostalgia/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        print("trigger");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("collision");
+    }
+}
