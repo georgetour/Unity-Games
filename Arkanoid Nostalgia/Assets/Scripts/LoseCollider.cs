1e5ea20d3f891c89c27648f2457c73fc4db77383
George Tourtsinakis <george@besmartbesimple.com>
2018-01-02 22:06:22 +0200

    Arkanoid Nostalgia ball out of screen goes to lose scene

--- Arkanoid Nostalgia/Assets/Scripts/LoseCollider.cs
+++ Arkanoid Nostalgia/Assets/Scripts/LoseCollider.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class LoseCollider : MonoBehaviour {
 
+    //Access to level manager
+    public LevelManager levelmanager ;
+
+    //When ball hits bottom return to main menu
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        print("trigger");
+        levelmanager.LoadLevel("Win-Lose");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
