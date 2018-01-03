1d467167079c9addea1e71bf2f9f067705d37af4
George Tourtsinakis <george@besmartbesimple.com>
2018-01-03 19:59:50 +0200

    Arcanoid nostalgia paddle and movement
    - Added the bottom paddle that we will control and the movement

---
+++ Arkanoid Nostalgia/Assets/Scripts/Paddle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //Get mouse position in game units
+        float mousePosInBlocks = Input.mousePosition.x / Screen.width * 20;
+
+        //Limit left right so it doesn't leave the screen
+        float maxLeftRightPosition = Mathf.Clamp(mousePosInBlocks, 0.63f, 18.65f);
+
+        //Set where the paddle starts
+        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
+        
+        
+        //Set mouse position horizontally
+        paddlePos.x = maxLeftRightPosition;
+
+        this.transform.position = paddlePos;
+	}
+}
