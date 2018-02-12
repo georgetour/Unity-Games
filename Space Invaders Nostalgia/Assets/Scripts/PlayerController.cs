1d05d353fcabc9564a3c4ac7184c52b96f847dae
George Tourtsinakis <george@besmartbesimple.com>
2018-02-12 21:34:49 +0200

    Movement added for space invaders
    - Ship moves
    
    TODO Limit ship to leave screen

--- Space Invaders Nostalgia/Assets/Scripts/PlayerController.cs
+++ Space Invaders Nostalgia/Assets/Scripts/PlayerController.cs
@@ -5,20 +5,44 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
+    //Move speed
+    public float shipSpeed = 15f;
+
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        StartingPosition();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            print("RIGHT ARROW PRESSED");
-            //Debug.Log("RIGHT ARROW PRESSED");
-        }
+        
+        MoveWithArrows();
 	}
+
+
+    //Move ship right and left
+    void MoveWithArrows()
+    {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(shipSpeed *Time.deltaTime, 0, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-shipSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+
+    void StartingPosition()
+    {
+        //Starting position
+        Vector3 shipStartingPos = new Vector3(0f, -4f, 1f);
+
+        this.transform.position = shipStartingPos;
+    }
 
    
 }
