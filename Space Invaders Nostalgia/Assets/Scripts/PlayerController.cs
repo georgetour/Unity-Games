bc1ca4b283ab52d916cbc40458aca2454026c109
George Tourtsinakis <george@besmartbesimple.com>
2018-02-14 17:45:57 +0200

    Movement restriction in Space Invaders Nostalgia

--- Space Invaders Nostalgia/Assets/Scripts/PlayerController.cs
+++ Space Invaders Nostalgia/Assets/Scripts/PlayerController.cs
@@ -6,12 +6,19 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour {
 
     //Move speed
-    public float shipSpeed = 15f;
+    public float speed = 10f;
+    public float padding = 1f;
+
+    float xmin;
+    float xmax; 
+
+
 
 
 	// Use this for initialization
 	void Start () {
         StartingPosition();
+        RestrictPosition();
     }
 	
 	// Update is called once per frame
@@ -24,16 +31,28 @@ public class PlayerController : MonoBehaviour {
     //Move ship right and left
     void MoveWithArrows()
     {
-
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(shipSpeed *Time.deltaTime, 0, 0);
+            //transform.position += new Vector3(speed *Time.deltaTime, 0, 0);
+            transform.position += Vector3.right * speed * Time.deltaTime;
+           
         }
-
         else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position += new Vector3(-shipSpeed * Time.deltaTime, 0, 0);
-        }
+            //transform.position += new Vector3(-shipSpeed * Time.deltaTime, 0, 0);
+            transform.position += Vector3.left * speed * Time.deltaTime;
+
+        //Restrict player from leaving boundaries
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+    }
+
+    void RestrictPosition()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmin = leftMost.x + padding;
+        xmax = rightMost.x - padding;
     }
 
     void StartingPosition()
