49d75e6afd25929107207b6e302a2a1ee0497255
George Tourtsinakis <george@besmartbesimple.com>
2018-02-17 23:02:17 +0200

    Added Enemy formation to spawn in Positions
    - Enemies spawn in gizmos we have made that are positions
    - Gizmos
    - OnDrawGizmo
    - Creating multiple objects for their parents

---
+++ Space Invaders Nostalgia/Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+        foreach (Transform child in transform)
+        {
+            //Spawn new enemy for every child EnemySpwane has
+            //And keep hierarchy in editor and the Instantiation wil be a GameObject
+            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+
+            //Make parent wherever this script is attached to which is the Position
+            enemy.transform.parent =child;
+        }
+
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
