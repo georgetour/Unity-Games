03388612d667c3b9b500aa41269b50036403e0dd
George Tourtsinakis <george@besmartbesimple.com>
2018-04-13 16:51:25 +0300

    Glitch Garden AddSplash screen and Start Menu
    Glitch Garden
    - Add splash screen and start menu
    
    ***GUIDE***
    - Add script so images become bigger onclick and not on hover as it was
    - Some minor fixes so imges appear below paragraphs

---
+++ Glitch Garden/Assets/Scripts/General/LevelManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+   
+    Scene currentScene;
+    string sceneName;
+
+    float secondsToLoad = 5;
+
+    private void Start()
+    {
+        
+        currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
+
+        //Load start screen after splassh screen some seconds
+        if (sceneName == "Splash")
+        {
+            StartCoroutine(CountDown());
+        }
+    }
+
+
+    public void LoadLevel(string name)
+	{ 
+         
+        //Load level according to parameter
+        Application.LoadLevel(name);
+
+ 
+    }
+
+	public void QuitRequest()
+	{
+		Application.Quit ();
+	}
+
+    public void LoadNextLevel()
+    {
+        
+        SceneManager.LoadScene(currentScene.buildIndex + 1);
+    }
+
+    IEnumerator CountDown()
+    {
+        yield return new WaitForSeconds(secondsToLoad);
+        LoadNextLevel();
+    }
+
+
+    }
