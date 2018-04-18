955a99e1a0671cc6d336c59fa7a9c37570350f7f
George Tourtsinakis <george@besmartbesimple.com>
2018-04-18 17:00:03 +0300

    Change music according to level Glitch Garden

---
+++ Glitch Garden/Assets/Scripts/General/MusicManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray;
+
+    private AudioSource audioSource;
+    
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+ 
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+
+    //Play music according to level and our public AudioClip array clip
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        AudioClip thisLevelMusic = levelMusicChangeArray[scene.buildIndex];
+        if (thisLevelMusic)
+        {
+            Debug.Log("start music");
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+            
+           
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
