eb091a553206ad024e222d6d9436165ee58c6326
George Tourtsinakis <george@besmartbesimple.com>
2018-04-20 21:35:52 +0300

    Add FadeIn to Start Scene Glitch Garden

---
+++ Glitch Garden/Assets/Scripts/General/Fading.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fading : MonoBehaviour {
+
+    public float fadeTime;
+
+    private Image fadePANEL;
+    private Color currentColor = Color.black;
+
+
+	// Use this for initialization
+	void Start () {
+
+        fadePANEL = GetComponent<Image>();
+   
+	}
+
+    private void Update()
+    {
+        if (Time.timeSinceLevelLoad < fadeTime)
+        {
+            fadeIn();
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+
+    //This fades the panel so the scene appears smoothly and not suddelnly
+    void fadeIn()
+    {
+        //Fade In
+        float alphaChange = Time.deltaTime / fadeTime;
+        //Change alpha
+        currentColor.a -= alphaChange;
+        fadePANEL.color = currentColor;
+
+    }
+
+
+	
+
+   
+}
